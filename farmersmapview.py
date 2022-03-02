c324b37687f13d1d43469c59be1113f8159c6146
seadz <sydneychirchir@gmail.com>
2022-03-02 16:34:19 +0300

    Day 1 serious work not finnished

--- farmersmapview.py
+++ farmersmapview.py
@@ -1,4 +1,23 @@
-from kivy_garden.mapview import MapView
 
+from kivy_garden.mapview import MapView
+from kivy.clock import Clock
+from kivy.app import App
 class FarmersMapView(MapView):
-    pass
+    getting_market_timer = None
+    def start_getting_markets_in_fov(self):
+        # after 1s get the markets in the field of view
+        try:
+            self.getting_market_timer.cancel()
+        except:
+            pass
+        self.getting_market_timer = Clock.schedule_once(self.get_markets_in_fov, 1)
+
+    def get_markets_in_fov(self, *args):
+        print(self.get_bbox())
+        min_lat, max_lat, min_lon, max_lon = self.get_bbox()
+        # Get refrence to the Main app and the database cursor
+        app = App.get_running_app()
+        sql_statement = 'SELECT * FROM markets WHERE lat > %s AND lat < %s AND lon > %s AND lon < %s'%(min_lat, max_lat, min_lon, max_lon)
+        app.cursor.execute(sql_statement)
+        markets = app.cursor.fetchall()
+        print(sql_statement)
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from kivymd.app import MDApp
 from farmersmapview import FarmersMapView
+import sqlite3 # He were importing the sqlite3 database
+
+# This is the main class that the app runs
 class MainApp(MDApp):
-    pass
+    connection  = None
+    cursor = None
+    def on_start(self):
+
+    # initialize local database connection
+        self.connection = sqlite3.connect("kenya.db")
+        self.cursor = self.connection.cursor()
+    # initialize GPS connection
+
+    
+    # Instantiatin local SearchPopupMenu
+   
+        
+
 
 MainApp().run()
